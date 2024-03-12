a499ee8ae9f541d9486c669a059b6953f53a6d22
dkormann <dominikormann@outlook.com>
2024-03-12 20:59:35 +0100

    channel

--- src/telegramapi.py
+++ src/telegramapi.py
@@ -4,9 +4,9 @@ import dotenv
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
+import asyncio
+
 telegram_token = dotenv.get_key("./.env","TELEGRAM_TOKEN")
-
-
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
@@ -17,6 +17,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         rf"Hi {user.mention_html()}!",
         reply_markup=ForceReply(selective=True),
     )
+    await application.bot.sendMessage(-1002035785054, "Bot started")
+    
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -24,14 +26,15 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     await update.message.reply_text("Help!")
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     await update.message.reply_text(update.message.text)
 
-
 def main() -> None:
+  
     """Start the bot."""
     # Create the Application and pass it your bot's token.
+    global application
     application = Application.builder().token(telegram_token).build()
 
     # on different commands - answer in Telegram
@@ -39,11 +42,13 @@ def main() -> None:
     application.add_handler(CommandHandler("help", help_command))
 
     # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
+
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
+
 if __name__ == "__main__":
-    main()
+  main()
