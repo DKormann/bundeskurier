3aa333012f182dbf31276046241177cc1da68669
dkormann <dominikormann@outlook.com>
2024-03-12 21:28:47 +0100

    move + mistral

--- src/llm.py
+++ src/llm.py
@@ -7,27 +7,23 @@ MISTRAL_API_KEY = dotenv.get_key("./.env","MISTRAL_API_KEY")
 
 
 def fetch_answer(prompt:str):
-    url = "https://api.mistral.ai/v1/chat/completions"
-    headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": f"Bearer {MISTRAL_API_KEY}"
-    }
-    data = {
-        "model": "mistral-large-latest",
-        "messages": [
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ]
-    }
-    response = requests.post(url, headers=headers, json=data)
-    data = response.json()
-    resp = data['choices'][0]['message']['content']
-    print(resp)
-    return resp
+  return chat_complete([{"role": "system", "content": prompt}])
 
+def chat_complete(messages: list):
+  url = "https://api.mistral.ai/v1/chat/completions"
+  headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "Authorization": f"Bearer {MISTRAL_API_KEY}"
+  }
+  data = {
+    "model": "mistral-large-latest",
+    "messages": messages
+  }
+  response = requests.post(url, headers=headers, json=data)
+  data = response.json()
+  resp = data['choices'][0]['message']['content']
+  return resp
 
 if __name__ == "__main__":
     print(fetch_answer("What is the meaning of life?"))
